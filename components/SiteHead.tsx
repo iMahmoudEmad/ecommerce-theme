02590409bd07fb9c357303d0e9de37c2fbf7cf54
Mahmoud Emad Eldin <memad@kashier.io>
2023-04-24 20:41:44 +0200

    Prettier all files

--- components/SiteHead.tsx
+++ components/SiteHead.tsx
@@ -11,7 +11,10 @@ export const SiteHead = () => {
       <meta name="colorScheme" content="light" />
       <meta name="creator" content={author} />
       <meta name="publisher" content="Vercel" />
-      <meta name="keywords" content="ecommerce, theme, ecommerce nextjs theme" />
+      <meta
+        name="keywords"
+        content="ecommerce, theme, ecommerce nextjs theme"
+      />
       <meta name="theme-color" content="#fff" />
       <meta name="generator" content="Nextjs 13.2.4" />
 
@@ -21,13 +24,19 @@ export const SiteHead = () => {
 
       <meta property="og:url" content="https://mahmoudemad.vercel.com" />
       <meta property="og:title" content={author} />
-      <meta property="og:description" content="ecommerce, theme, ecommerce nextjs theme" />
+      <meta
+        property="og:description"
+        content="ecommerce, theme, ecommerce nextjs theme"
+      />
       <meta property="og:image" content="https://mahmoudemad.vercel.com/og" />
       <meta property="og:type" content="website" />
 
       <meta name="apple-mobile-web-app-title" content={author} />
       <link rel="apple-touch-icon" href="/assets/images/mahmoud.jpg" />
-      <meta name="description" content="ecommerce, theme, ecommerce nextjs theme" />
+      <meta
+        name="description"
+        content="ecommerce, theme, ecommerce nextjs theme"
+      />
 
       <title>Ecommerce theme</title>
       <link rel="shortcut icon" href="/favicon.ico" />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,6 @@
-import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
-// import dynamic from 'next/dynamic';
-
-// const PwaUpdater = dynamic(() => import(`./PwaUpdater`), { ssr: false });
+import "@/styles/globals.scss";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      {/* <PwaUpdater /> */}
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />;
 }
