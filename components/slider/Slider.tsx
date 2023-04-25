2aaf747626dea744155ea24d54699024c97fde8b
Mahmoud Emad Eldin <memad@kashier.io>
2023-04-25 16:16:46 +0200

    Changed some stuff on slider component

--- components/slider/Slider.tsx
+++ components/slider/Slider.tsx
@@ -1,40 +1,26 @@
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-import Link from "next/link";
-import Image from "next/image";
-import styles from "./Slider.module.scss";
+import { Navigation, Pagination, A11y } from "swiper";
+import { Swiper } from "swiper/react";
 
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+import "swiper/css/a11y";
 
-export const Slider = () => {
+export const Slider = (props:any) => {
   return (
     <Swiper
       // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      modules={[Navigation, Pagination, A11y]}
       spaceBetween={0}
       slidesPerView={1}
       navigation
       pagination={{ clickable: true }}
       onSwiper={(swiper: any) => console.log(swiper)}
       onSlideChange={() => console.log("slide change")}
+      loop={props?.loop}
     >
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((slide) => (
-        <SwiperSlide key={slide}>
-          <Link href="#">
-            <Image
-              src="/assets/images/slider.png"
-              alt="title"
-              width={1000}
-              height={100}
-              className={styles.sliderImage}
-            />
-          </Link>
-        </SwiperSlide>
-      ))}
+      {props.children}
     </Swiper>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,25 @@ import Navbar from "@/components/navbar/Navbar";
 import { CardC } from "@/components/card/Card.constant";
 import CardI from "@/components/card/Card.interface";
 import { Slider } from "@/components/slider/Slider";
+import SliderI from "@/components/slider/Slider.interface";
+import { SliderC } from "@/components/slider/Slider.constant";
+import { v4 } from "uuid";
+import Link from "next/link";
+import Image from "next/image";
+import sliderStyles from "@/components/slider/Slider.module.scss";
+import { SwiperSlide } from "swiper/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const cards: CardI[] = [CardC, CardC, CardC];
+const slides: SliderI[] = [
+  SliderC,
+  SliderC,
+  SliderC,
+  SliderC,
+  SliderC,
+  SliderC,
+];
 
 export default function Home() {
   return (
@@ -17,11 +32,25 @@ export default function Home() {
       <SiteHead />
       <Navbar />
 
-      <Slider />
+      <Slider loop={true}>
+        {slides.map((slide: SliderI) => (
+          <SwiperSlide key={v4()}>
+            <Link href={slide?.link}>
+              <Image
+                src={slide?.image}
+                alt={slide?.name}
+                width={1000}
+                height={100}
+                className={sliderStyles.sliderImage}
+              />
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Slider>
 
       <main className={`${inter.className} ${styles.main}`}>
         {cards?.map((card, idx) => (
-          <Card card={card} key={`${idx}-${card?.id}`} />
+          <Card card={card} key={v4()} />
         ))}
       </main>
     </>
